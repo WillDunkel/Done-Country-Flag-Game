717c0aa59cba47e7ef14a0503f0577988cd41012
William Dunkel <wndunkel@gmail.com>
2024-02-28 09:43:08 -0600

    Initial Commit

---
+++ Done Country Flag Game/Done_Country_Flag_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Done_Country_Flag_GameApp.swift
+//  Done Country Flag Game
+//
+//  Created by William Dunkel on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Done_Country_Flag_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
